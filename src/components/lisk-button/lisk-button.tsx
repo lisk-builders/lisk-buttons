f10ee5cb620eef527cc34880d9b3d57cce63d655
Aleksey Popov <alepooop@gmail.com>
2018-07-10 15:02:12 +0300

    Redirect on error
    When onError callback of openUrl is calling, buttons will redirect end
    user to sites based on his user agent to download Lisk Hub or mobile Hub

--- src/components/lisk-button/lisk-button.tsx
+++ src/components/lisk-button/lisk-button.tsx
@@ -1,24 +1,37 @@
-import { Component, State } from '@stencil/core';
+import { Component } from '@stencil/core';
 
-import { openURL as checkAndOpen } from '../utils/index';
+import {
+  AppUrls,
+  appLinks,
+  mobile,
+  openURL as checkAndOpen,
+} from '../utils/index';
 
 @Component({
   tag: 'lisk-button',
 })
 export class LiskButton {
-  @State() loading = false;
+  private urls: AppUrls = appLinks;
 
   openUrl(url: string): void {
-    this.loading = true;
     checkAndOpen(url, this.onError, this.onSuccess);
   }
 
-  private onSuccess = (): void => {
-    this.loading = false;
-  }
+  private onSuccess(): void {} // tslint:disable-line
 
   private onError = (): void => {
-    this.loading = false;
+    let redirectUrl = this.urls.desktop;
+
+    // Redirect to the mobile app store
+    if (mobile.isAndroid && this.urls.android) {
+      redirectUrl = this.urls.android;
+    }
+
+    if (mobile.isIOS && this.urls.ios) {
+      redirectUrl = this.urls.ios;
+    }
+
+    window.location.href = redirectUrl;
   }
 
   hostData(classNames = '') {
--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -11,27 +11,52 @@ interface SendLinkParams {
 interface VoteLinkParams {
   kind: 'vote';
   votes?: string;
-  unvotes?: string
+  unvotes?: string;
 }
 
 interface SignLinkParams {
   kind: 'sign-hub' | 'sign-nano';
-  message?: string
+  message?: string;
 }
 
 type URLParams = SendLinkParams | VoteLinkParams | SignLinkParams;
 
 export const getURL = (params: URLParams) => {
-  switch(params.kind) {
+  switch (params.kind) {
     case 'send':
-      return `${liskURL}main/transactions/send?recipient=${params.recipient}&amount=${params.amount}`;
+      return `${liskURL}main/transactions/send?recipient=${
+        params.recipient
+      }&amount=${params.amount}`;
     case 'vote':
-      return `${liskURL}main/voting/vote?${params.unvotes ? `unvotes=${params.unvotes}` : ''}&${params.votes ? `votes=${params.votes}` : ''}`;
-    case 'sign-nano': //there is no support to message pre-fill on lisk-nano...
+      return `${liskURL}main/voting/vote?${
+        params.unvotes ? `unvotes=${params.unvotes}` : ''
+      }&${params.votes ? `votes=${params.votes}` : ''}`;
+    case 'sign-nano': // there is no support to message pre-fill on lisk-nano...
       return `${liskURL}main/transactions/sign-message`;
     case 'sign-hub':
-      return `${liskURL}sign-message?${params.message ? `message=${params.message}` : ''}`;
+      return `${liskURL}sign-message?${
+        params.message ? `message=${params.message}` : ''
+      }`;
   }
 };
 
-export const openURL = (uri: string, failCb: Function, successCb: Function) => checkURI(uri, failCb, successCb);
+export const openURL = (
+  uri: string,
+  failCb: () => void,
+  successCb: () => void
+) => checkURI(uri, failCb, successCb);
+
+export const mobile = {
+  isIOS: /iPhone|iPad|iPod/i.test(navigator.userAgent),
+  isAndroid: /Android/i.test(navigator.userAgent),
+};
+
+export interface AppUrls {
+  ios?: string;
+  android?: string;
+  desktop: string;
+}
+
+export const appLinks: AppUrls = {
+  desktop: 'https://www.lisk.io/hub',
+};
