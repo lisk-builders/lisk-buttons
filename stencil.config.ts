87d0a48bda70ec26a7e6eda82f401d1a8e4d5828
Aleksey Popov <alepooop@tgmail.com>
2019-04-06 00:13:40 +0300

    update stencil core to 0.18.1

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,17 +1,16 @@
-const sass = require('@stencil/sass');
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
-exports.config = {
+export const config: Config = {
   bundles: [
     { components: ['lisk-button-send', 'lisk-button-vote', 'lisk-button-sign'] }
+  ],
+  plugins: [
+    sass(),
   ],
   namespace: 'liskButtons',
   outputTargets: [
     { type: 'www' },
-    { type: 'dist' }
+    { type: 'dist' },
   ],
 };
-
-exports.devServer = {
-  root: 'www',
-  watchGlob: '**/**'
-}
